24743647aa6a233e99c0a7f446f641b9ef56988e
pietroferridelagos <piferri@hotmail.com>
2024-11-27 16:51:51 -0300

    commit test

---
+++ src/Paciente.java
@@ -0,0 +1,79 @@
+public class Paciente {
+
+    private String nome;
+    private String cpf;
+    private String dataNascimento;
+    private String endereco;
+    private String telefone;
+    private String email;
+
+    // Construtor
+    public Paciente(String nome, String cpf, String dataNascimento, String endereco, String telefone, String email) {
+        this.nome = nome;
+        this.cpf = cpf;
+        this.dataNascimento = dataNascimento;
+        this.endereco = endereco;
+        this.telefone = telefone;
+        this.email = email;
+    }
+
+    // Getters e Setters
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(String dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    // Método para exibir informações do paciente
+
+    public void exibirInformacoes() {
+        System.out.println("Nome: " + nome);
+        System.out.println("CPF: " + cpf);
+        System.out.println("Data de Nascimento: " + dataNascimento);
+        System.out.println("Endereço: " + endereco);
+        System.out.println("Telefone: " + telefone);
+        System.out.println("E-mail: " + email);
+    }
+}
--- src/SistemadeMonitoramento.java
+++ src/SistemadeMonitoramento.java
@@ -1,6 +1,6 @@
 public class SistemadeMonitoramento {
 
     public static void main(String[] args) {
-        System.out.println("Hello World");
+
     }
 }
