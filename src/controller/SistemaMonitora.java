8b1f9f253024eb12192f58b77f525bc818e848ff
pietroferridelagos <piferri@hotmail.com>
2024-11-28 15:34:25 -0300

    commit test

--- src/controller/SistemaMonitora.java
+++ src/controller/SistemaMonitora.java
@@ -1,4 +1,6 @@
-public class SistemadeMonitoramento {
+package controller;
+
+public class SistemaMonitora {
 
     public static void main(String[] args) {
 
--- src/model/Medico.java
+++ src/model/Medico.java
@@ -1,3 +1,5 @@
+package model;
+
 public class Medico {
     private String nome;
     private String especialidade;
